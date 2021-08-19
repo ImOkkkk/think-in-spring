00fc510744bd0099d2562934d5b64e40c8198c17
ImOkkkk <547717253@qq.com>
2021-08-19 22:17:54 +0800

    feat(延迟依赖查找): ObjectProvider

--- dependency-lookup/src/main/java/org/liuwy/dependecy/lookup/ObjectProviderDemo.java
+++ dependency-lookup/src/main/java/org/liuwy/dependecy/lookup/ObjectProviderDemo.java
@@ -1,9 +1,11 @@
 package org.liuwy.dependecy.lookup;
 
+import org.liuwy.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 通过{@link org.springframework.beans.factory.ObjectProvider}进行依赖查找
@@ -19,12 +21,36 @@ public class ObjectProviderDemo { /// @Configuration是非必须的
         applicationContext.register(ObjectProviderDemo.class);
         applicationContext.refresh();
         lookupByObjectProvider(applicationContext);
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         applicationContext.close();
     }
 
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        /*Iterable<String> stringIterable = objectProvider;
+        for (String string : stringIterable) {
+            System.out.println(string);
+        }*/
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        // 如果不存在，默认创建user
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前User对象：" + user.toString());
+    }
+
     @Bean
+    @Primary
     public String helloWord() { // 方法名就是Bean名称
         return "Hello World!";
+    }
+
+    @Bean
+    public String message() {
+        return "message";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
